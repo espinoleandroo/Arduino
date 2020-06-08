3346f548af0168beb0305ce97cbad4dca4d24b7b
Leandro Espino <espinoleandroo@gmail.com>
2020-06-07 22:33:22 -0500

    Primeras practicas con leds

---
+++ Digital_Inputs/Digital_Inputs.ino
@@ -0,0 +1,25 @@
+//www.elegoo.com
+//2016.12.08
+
+int ledPin = 8;
+int buttonApin = 11;
+int buttonBpin = 10;
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonApin, INPUT_PULLUP);  
+  pinMode(buttonBpin, INPUT_PULLUP);  
+}
+
+void loop() 
+{
+  if (digitalRead(buttonApin) == LOW)
+  {
+    digitalWrite(ledPin, HIGH);
+  }
+  if (digitalRead(buttonBpin) == LOW)
+  {
+    digitalWrite(ledPin, LOW);
+  }
+}
